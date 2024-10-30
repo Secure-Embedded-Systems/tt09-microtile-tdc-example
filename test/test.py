912b37ea780e8b67cca3b94e8bed13696a4520bb
Roy1707018 <aroy1@wpi.edu>
2024-10-30 17:50:31 -0400

    microtile experiment

--- test/test.py
+++ test/test.py
@@ -3,11 +3,11 @@
 
 import cocotb
 from cocotb.clock import Clock
-from cocotb.triggers import ClockCycles, FallingEdge
+from cocotb.triggers import ClockCycles
 
 
 @cocotb.test()
-async def test_proj1(dut):
+async def test_project(dut):
     dut._log.info("Start")
 
     # Set the clock period to 10 us (100 KHz)
@@ -18,23 +18,30 @@ async def test_proj1(dut):
     dut._log.info("Reset")
     dut.ena.value = 1
     dut.ui_in.value = 0
-    dut.uio_in.value = 0  # Select the first project, echos the input on reset and outputs a counter otherwise
+    dut.uio_in.value = 0
     dut.rst_n.value = 0
     await ClockCycles(dut.clk, 10)
-
-    dut._log.info("Test input echo on reset")
-
-    assert dut.uo_out.value == 0
-    for x in range(256):
-        dut.ui_in.value = x
-        await ClockCycles(dut.clk, 1)
-        await FallingEdge(dut.clk)
-        assert dut.uo_out.value == x
-
-    dut._log.info("Test counter")
     dut.rst_n.value = 1
-    await ClockCycles(dut.clk, 1)
-    for x in range(256):
-        await FallingEdge(dut.clk)
-        assert dut.uo_out.value == x
-        await ClockCycles(dut.clk, 1)
+
+    dut._log.info("Test project behavior")
+
+    await ClockCycles(dut.clk,1)
+    dut._log.info("TDC check")
+    dut.ui_in.value = 3
+    await ClockCycles(dut.clk,1)
+    dut.ui_in.value = 2
+    # Read and log the output value
+    output_value = dut.uo_out.value
+    dut._log.info(f"TDC Output value: {output_value}")
+
+    await ClockCycles(dut.clk,2)
+    dut._log.info("TDC check")
+    dut.ui_in.value = 165
+    await ClockCycles(dut.clk,2)
+    dut.ui_in.value = 229
+    # Read and log the output value
+    output_value = dut.uo_out.value
+    dut._log.info(f"TDC Output value: {output_value}")
+    await ClockCycles(dut.clk, 4)
+
+    
