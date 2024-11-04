96050c2116405cb33c58df19c0a3b4de5b34376e
Arna Roy <arnaroy@Arnas-Laptop.local>
2024-11-03 23:11:45 -0500

    update

--- test/tb.v
+++ test/tb.v
@@ -18,10 +18,10 @@ module tb ();
   reg rst_n;
   reg ena;
   reg [7:0] ui_in;
-  //reg [7:0] uio_in;
+  reg [7:0] uio_in;
   wire [7:0] uo_out;
-  //wire [7:0] uio_out;
-  // wire [7:0] uio_oe;
+  wire [7:0] uio_out;
+  wire [7:0] uio_oe;
 
   // Declare power and ground wires outside procedural blocks
   wire vpwr;
@@ -40,6 +40,9 @@ module tb ();
     `endif
       .ena(ena),
       .clk(clk),
+      .uio_in (uio_in),   // IOs: Input path
+      .uio_out(uio_out),  // IOs: Output path
+      .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
       .ui_in  (ui_in),    // Dedicated inputs
       .uo_out (uo_out),   // Dedicated output
       .rst_n  (rst_n)     // Active-low reset
